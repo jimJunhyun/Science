6f77319843e678434821706eda3778983a426cb2
jimJunhyun <tsumego2015@gmail.com>
2022-09-20 00:49:10 +0900

    finalization with start and end

---
+++ ScienceGame/Assets/Scripts/SceneCtrkl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneCtrkl : MonoBehaviour
+{
+	public static SceneCtrkl instance;
+    public string targetSceneName;
+	private void Awake()
+	{
+		instance = this;
+	}
+	public void Loader()
+	{
+		SceneManager.LoadScene(targetSceneName);
+	}
+}
